36aa618cdcf9a2243853bfd5180d6ed5bceccc34
dtamon <dominiktamon@gmail.com>
2022-09-20 14:09:30 +0200

    Add project files.

---
+++ ReverseStringMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReverseStringMVCApp.Models;
+using System.Diagnostics;
+
+namespace ReverseStringMVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new StringModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(StringModel _stringModel)
+        {
+            _stringModel.Reversed = Reverse(_stringModel.ToReverse);
+            return View(_stringModel);
+        }
+
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        static string Reverse(String str)
+        {
+            if (str.Length > 0)
+                return str[str.Length - 1] + Reverse(str.Substring(0, str.Length - 1));
+            else
+                return str;
+        }
+    }
+}
---
+++ ReverseStringMVCApp/Models/StringModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ReverseStringMVCApp.Models
+{
+    public class StringModel
+    {
+        public string ToReverse { get; set; }
+        public string Reversed { get; set; }
+    }
+}
